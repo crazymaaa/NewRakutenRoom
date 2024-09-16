b9bd794e45968ed73c45da5f88bc1ef6e1b61933
crazyma-nn <iwn001bim0@gmail.com>
2024-09-16 15:24:40 +0900

    32asf23wafa

--- index.js
+++ index.js
@@ -55,8 +55,8 @@ const test = async(age) => {
 async function post(itemCode, description, itemName, catchcopy, browser) {
   try {
 
-    // const page = await browser.newPage();
-    const page = await browser.newPage("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36");
+    const page = await browser.newPage();
+    // const page = await browser.newPage("Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/111.0.0.0 Safari/537.36");
     // await page.setUserAgent(
     //   "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
     // );
